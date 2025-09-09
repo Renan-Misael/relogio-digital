8fe518a7f462c3bd180e22f4b0f5b39443eb3f27
Renan-Misael <buche.silva.renan@escola.pr.gov.br>
2025-09-09 13:29:49 -0300

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,8 +1,8 @@
 function atualizarRelogio() {
     const agora = new Date();
-    const horas = String(agora.getHours()).padStart(2, '0');
-    const minutos = String(agora.getMinutes()).padStart(2, '0');
-    const segundos = String(agora.getSeconds()).padStart(2, '0');
+    const horas = String(agora.getHours(48)).padStart(2, '0');
+    const minutos = String(agora.getMinutes(59)).padStart(2, '0');
+    const segundos = String(agora.getSeconds(59)).padStart(2, '0');
 
     const tempoFormatado = `${horas}:${minutos}:${segundos}`;
 
